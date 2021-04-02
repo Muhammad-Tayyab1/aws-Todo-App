b77f93152aa988e19f42e8494af3223cb80367c5
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2021-04-02 12:32:40 +0500

    update

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -4,10 +4,10 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 export const client = new ApolloClient({
   link: new HttpLink({
     uri:
-      " https://3yb4blwpqnawvi3wnfcbwso5ia.appsync-api.us-west-2.amazonaws.com/graphql", // ENTER YOUR GRAPHQL ENDPOINT HERE
+      " ", // ENTER YOUR GRAPHQL ENDPOINT HERE
     fetch,
     headers: {
-      "x-api-key": "da2-dwmk6nxukzaf5kgqpem6uuglna", // ENTER YOUR API KEY HERE
+      "x-api-key": "", // ENTER YOUR API KEY HERE
     },
   }),
   cache: new InMemoryCache(),
